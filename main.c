7c312ceb7276fb4369ef083e994e3cbd841a36b8
W9D10 <damian.otazu91@gmail.com>
2026-02-11 21:24:04 -0300

    Se completo la funcion eval, es una asquerosidad pero funciona

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <stdbool.h>
 #include <string.h>
 #include "parser.h"
-
+#include <stdlib.h>
 
 
 #define CERO 48
+#define BASE 10
 
 int sum(int a, int b){
     return a + b;
@@ -20,15 +21,15 @@ int mul(int a,int b){
 }
 
 int division(int a,int b){
-    if(b < 1){
-        fprintf(stderr,"ERROR: La division por cero no esta definida\n");
+    if(b == 0){
+        return fprintf(stderr,"ERROR: La division por cero no esta definida\n");
     }
     return a / b;
 }
 
 int pot(int num,int exp){
     if(exp < 0){
-        fprintf(stderr,"ERROR: No se pueden calcular exponentes negativos\n");
+        return fprintf(stderr,"ERROR: No se pueden calcular exponentes negativos\n");
     }
     int n = num;
     if(exp == 0){
@@ -44,18 +45,18 @@ int pot(int num,int exp){
 
 int raiz(int a,int b){
     int resul = -1;
-    fprintf(stderr,"ERROR: No implementado\n");
+    return fprintf(stderr,"ERROR: No implementado\n");
     return resul;
 }
 
+//conviente una cadena a un valor entero
 int to_int(char n[]){
     int num = 0;
-    int base = 10;
     int len = strlen(n);
     for(int i = 0; i < len; i++){
         //CERO es en numero 48 del codigo ascii al restarselo a los caracteres del 0..9 
         //puedo transformarlos en enteros.
-       num += pot(base,len - 1 - i) * (n[i] - CERO);
+       num += pot(BASE,len - 1 - i) * (n[i] - CERO);
     }
     return num;
 }
@@ -82,38 +83,88 @@ int ejecutar_operacion(char op,int n1,int n2){
     return resul;
 }
 
-void eval(token_t *token){
+
+//Toma un nuimero y devielve su logitud, es decir la cantidad de digitos
+//por los que estï¿½ formado.
+int int_len(int num){
+    if(num == 0) return 1;
+    if(num < 0) num *= -1;
+    int len = 0;
+    while(num != 0){
+        len++;
+        num /= BASE;
+    }
+    return len;
+}
+
+//Convierte un valor entero a una cadena de caracteres
+char* int_to_char(int num){
+    int len = int_len(num);
+    char *c = (char*)malloc(len + 1);
+    for(int i = 0; i < len; i ++){
+        c[len - 1 - i] = CERO + (num % BASE);
+        num /= BASE;
+    }
+    c[len] = '\0';
+    return c;
+}
+
+void liberar_nodo(token_t *nodo){
+    free(nodo->c);
+    free(nodo);
+}
+
+
+
+int eval(token_t *token){
     token_t *c = token;
     token_t *op = NULL;
-    token_t *num1 = NULL;
+    token_t *num1;
+    int resul;
     while(token != NULL){
         if(token->t == OPERADOR){
             op = token;
             num1 = NULL;
-        }else if(token->t == NUMERO && num1 == NULL){
-            num1 = token; 
-        }else if (token->t == NUMERO){
-            
+            token = token->sig;
+        } else if(token->t == NUMERO){
+            num1 = token;
+            token_t *n2 = num1->sig;
+            if(n2->t == NUMERO){
+                int rParcial = ejecutar_operacion(op->c[0],to_int(num1->c),to_int(n2->c));
+                op->sig = n2->sig;
+                free(op->c);
+                op->c = int_to_char(rParcial);
+                op->t = NUMERO;
+                liberar_nodo(num1);
+                liberar_nodo(n2);
+                if(op == c){
+                    if(op->sig == NULL){
+                        token = NULL;
+                        resul = to_int(op->c);
+                        liberar_nodo(op);
+                    }else{
+                        token = op->sig;
+                    }
+                }else{
+                    token = c;
+                }
+            }else if(n2->t == OPERADOR){
+                op = n2;
+                token = op->sig;
+            }
         }
     }
+    return resul;
 }
 
 int main(){
     char expr[256];
-    //No se me ocurrio ningún otro nombre
+    //No se me ocurrio ningï¿½n otro nombre
     token_t *token = NULL;
     printf("Ingrese expresion matematica: \n");
     fgets(expr,256,stdin);
     generar_tokens(&token,expr);
     token = a_pol(&token);
-    token_t *aux = token;
-    while(aux !=NULL){
-        printf("%s ",aux->c);
-        aux = aux->sig;
-	}
-
-    while(token != NULL){
-        token = liberar_mem(token);
-    }
+    printf("%d\n",eval(token));
     return 0;
   }
--- parser.h
+++ parser.h
@@ -5,5 +5,5 @@
 int jerarquia_op(char op[]);
 token_t* liberar_mem(token_t *l);
 bool es_mayor(int topeOp,int topeToken);
-void separadores(token_t *ops,token_t *pol,token_t **token);
+void separadores(token_t *ops,token_t **pol,token_t **token);
 token_t* a_pol(token_t **token);
